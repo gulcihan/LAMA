c6d03b3f5230d008d603bd86d2411c33447b5635
buyukdemircioglug <gulcihan.buyukdemircioglu@commencis.com>
2018-03-28 16:09:49 +0300

    Add MainActivity

---
+++ app/src/main/java/com/example/buyukdemircioglug/landslidealert/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.buyukdemircioglug.landslidealert;
+
+import android.support.v4.app.Fragment;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected Fragment getContainedFragment() {
+        return null;
+    }
+
+    @Override
+    protected boolean hasToolbar() {
+        return true;
+    }
+}
